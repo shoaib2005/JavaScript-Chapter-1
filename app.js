1be011d0c5987ee40bbbc3ed6290b7c5f2b6304c
shoaib2005 <shoaib.here@gmail.com>
2024-03-04 15:43:31 +0500

    First Chapter

---
+++ app.js
@@ -0,0 +1,28 @@
+// Question 1
+
+// alert("Hello and Welcome to our website");
+
+// //  Question 2
+
+// window.alert("JavaScript Alert \n\n Error! Please enter a valid password.");
+
+// // Question 3
+
+// window.alert("JavaScript Alert \n\n Welcome to JS Land... \n Happy Coding");
+
+// Question 4
+
+// window.alert("JavaScript Alert \n\n Welcome to JS Land");
+// window.alert("JavaScript Alert \n\n Happy Coding.\n Prevent this page from creating additional dialog.");
+
+// Question 5
+
+// window.alert("JavaScript Alert \n\n Hello... I can JS through my web browser's console");
+
+// Question 6
+
+alert("JavaScript Alert \n\n Hello and Welcome to my website.");
+
+// Question 7
+
+
